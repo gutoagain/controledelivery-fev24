2367e8e79b668ef5c498fbac978e824fe994dd09
Marcio <marcio.augusto.o.s@gmail.com>
2024-04-11 22:15:06 -0300

    MongoDB e Swagger adicionados - teste ok!

---
+++ src/main/java/dev/wakandaacademy/controledelivery/ControledeliveryApplication.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.controledelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControledeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControledeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wakandaacademy/controledelivery/ControledeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.controledelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControledeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
